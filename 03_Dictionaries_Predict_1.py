434b8b7e8b2e17b0596bd58aa1dc7cdb614d83ed
Trevor Swarm <tswarm@lccc.wy.edu>
2025-09-29 15:56:47 -0600

    Create 03_Dictionaries_Predict_1.py

---
+++ 03_Dictionaries_Predict_1.py
@@ -0,0 +1,20 @@
+# filename: 03_Dictionaries_Predict_1.py
+"""
+Activity: Predict the Output
+
+1.  Read the code below which defines a dictionary of student grades.
+2.  Without running the code, what do you think will be printed to the console?
+3.  Write your answer in the `prediction` variable.
+"""
+
+prediction = "" # Example: "A+" or 95
+
+student_grades = {
+    "Alice": "A",
+    "Bob": "B+",
+    "Charlie": "A-"
+}
+
+bobs_grade = student_grades["Bob"]
+
+print("Bob's grade is:", bobs_grade)
